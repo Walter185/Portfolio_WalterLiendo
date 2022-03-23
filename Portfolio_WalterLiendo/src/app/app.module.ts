172973c8c363378f549b68d9ab6f264695e20f06
Walter185 <walter.liendo@gmail.com>
2022-03-22 21:05:35 -0300

    footer

--- Portfolio_WalterLiendo/src/app/app.module.ts
+++ Portfolio_WalterLiendo/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftskills.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     ExperienciaComponent,
     EducacionComponent,
     HardsoftskillsComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
