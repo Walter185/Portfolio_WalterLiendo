e317ccb054b8cf8e221653c4e6cbdf07d445e71a
Walter185 <walter.liendo@gmail.com>
2022-04-23 16:10:11 -0300

    json-server

--- Front-End-Angular/src/app/services/portfolio.service.ts
+++ Front-End-Angular/src/app/services/portfolio.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Data } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get('./assets/data/data.json');
+    return this.httpClient.get('./assets/data/data.json');
+  }
+  saveData(data:Data): Observable<any> {
+    return this.httpClient.post('./assets/data/data.json', data);
   }
 }
