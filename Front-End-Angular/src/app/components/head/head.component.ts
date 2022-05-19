04ee01a040a669d6818f690e368d7c9d8df6dd02
Walter185 <walter.liendo@gmail.com>
2022-05-19 20:46:45 -0300

    contacto

--- Front-End-Angular/src/app/components/head/head.component.ts
+++ Front-End-Angular/src/app/components/head/head.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { persona } from 'src/app/model/persona.model';
 import { UserService } from 'src/app/_services/user.service';
+import { Routes } from '@angular/router';
 
 @Component({
   selector: 'app-head',
